d40bb828032a68e9da89df24da49b9f9b5d05113
AndreyGruzdov <andrj169@gmail.com>
2022-05-27 11:40:51 -0400

    'now'

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5323657881:AAFAj5wv-w3J8NzlGLprEzA6SkFpbcNTXJQ'
+
+#currencies = {
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
